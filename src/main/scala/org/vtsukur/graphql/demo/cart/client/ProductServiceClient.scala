2a8f0af7925fc71676bde7fb516edf8460530462
vtsukur <volodymyrt@wix.com>
2018-04-24 23:11:15 +0300

    Migrating to Akka HTTP backend for the product service client

--- src/main/scala/org/vtsukur/graphql/demo/cart/client/ProductServiceClient.scala
+++ src/main/scala/org/vtsukur/graphql/demo/cart/client/ProductServiceClient.scala
@@ -1,31 +1,35 @@
 package org.vtsukur.graphql.demo.cart.client
 
 import com.softwaremill.sttp.Uri.QueryFragment
-import com.softwaremill.sttp.circe.asJson
 import com.softwaremill.sttp._
+import com.softwaremill.sttp.akkahttp.AkkaHttpBackend
+import com.softwaremill.sttp.circe.asJson
 import com.typesafe.scalalogging.StrictLogging
 import io.circe.generic.auto._
 import org.vtsukur.graphql.demo.product.integration.domain.{ProductDto, ProductsDto}
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 class ProductServiceClient extends StrictLogging {
-  private implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
+  private implicit val backend = AkkaHttpBackend()
 
   private val baseUrl = "http://localhost:9090"
 
   def fetchProductById(id: String): ProductDto = {
-    sttp.get(uri"$baseUrl/api/products/$id")
+    Await.result(sttp.get(uri"$baseUrl/api/products/$id")
       .response(asJson[ProductDto])
-      .send()
+      .send(), 10.second)
       .unsafeBody.right.get
   }
 
   def fetchProductsByIds(ids: Seq[String], fields: Seq[String] = Seq.empty): ProductsDto = {
-    sttp.get(uri"$baseUrl/api/products"
+    Await.result(sttp.get(uri"$baseUrl/api/products"
       .queryFragment(QueryFragment.KeyValue("ids", ids.mkString(",")))
       .queryFragment(QueryFragment.KeyValue("include", fields.mkString(",")))
     )
       .response(asJson[ProductsDto])
-      .send()
+      .send(), 10.second)
       .unsafeBody.right.get
   }
 }
